bee67d1da90654aac040863e4572a0647abb3817
gyansagarwebsite <gyansagarwebsite@gmail.com>
2026-04-12 15:41:20 +0530

    Update adminSettingsService.js

--- frontend/src/admin/services/adminSettingsService.js
+++ frontend/src/admin/services/adminSettingsService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000/api';
+const BASE_URL = process.env.REACT_APP_API_URL || 'https://gyansagar.onrender.com/api';
 const API_URL = `${BASE_URL}/contact-settings`;
 const getContactSettings = async () => {
   const response = await axios.get(API_URL);
